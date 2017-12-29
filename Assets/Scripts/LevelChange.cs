5ef995add0aa3b0062147f190fc94ba43af18c86
Mikel Matticoli <mikel@null.net>
2017-12-29 18:34:56 -0500

    Added project files

---
+++ Assets/Scripts/LevelChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+        Scene currentScene = SceneManager.GetActiveScene();
+        int lvlNum = 0;
+        int.TryParse(currentScene.name.Split(' ')[1], out lvlNum);
+        SceneManager.LoadScene("Level " + (lvlNum+1));
+    }
+}
